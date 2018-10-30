9bb8f814b96629a98ab97360a0db7624a7ada56c
HuiChihWang <taya87136@gmail.com>
2018-10-30 06:58:58 +0000

    finish softmax classifier

--- assignments/2018/assignment1/cs231n/classifiers/linear_svm.py
+++ assignments/2018/assignment1/cs231n/classifiers/linear_svm.py
@@ -34,14 +34,17 @@ def svm_loss_naive(W, X, y, reg):
       margin = scores[j] - correct_class_score + 1 # note delta = 1
       if margin > 0:
         loss += margin
+        dW[:,j] += X[i]
+        dW[:,y[i]] -= X[i]
 
   # Right now the loss is a sum over all training examples, but we want it
   # to be an average instead so we divide by num_train.
   loss /= num_train
-
+  dW /= num_train
+    
   # Add regularization to the loss.
   loss += reg * np.sum(W * W)
-
+  dW += 2 * reg * W
   #############################################################################
   # TODO:                                                                     #
   # Compute the gradient of the loss function and store it dW.                #
@@ -50,8 +53,6 @@ def svm_loss_naive(W, X, y, reg):
   # loss is being computed. As a result you may need to modify some of the    #
   # code above to compute the gradient.                                       #
   #############################################################################
-
-
   return loss, dW
 
 
@@ -63,13 +64,21 @@ def svm_loss_vectorized(W, X, y, reg):
   """
   loss = 0.0
   dW = np.zeros(W.shape) # initialize the gradient as zero
-
+  num_train = X.shape[0]
   #############################################################################
   # TODO:                                                                     #
   # Implement a vectorized version of the structured SVM loss, storing the    #
   # result in loss.                                                           #
   #############################################################################
-  pass
+  scores = X @ W
+  correct_scores = scores[np.arange(num_train),y]
+  correct_scores = np.expand_dims(correct_scores, axis=1)
+  loss_temp = scores - correct_scores + 1
+  loss_temp[np.arange(num_train),y] = 0
+  loss_temp[loss_temp < 0] = 0
+  loss = np.sum(loss_temp)
+  loss /= num_train
+  loss += reg * np.sum(W * W)
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
@@ -84,7 +93,12 @@ def svm_loss_vectorized(W, X, y, reg):
   # to reuse some of the intermediate values that you used to compute the     #
   # loss.                                                                     #
   #############################################################################
-  pass
+  
+  loss_temp[loss_temp > 0] = 1
+  loss_temp[np.arange(num_train),y] = -1 * np.sum(loss_temp, axis = 1)
+  dW = X.T @ loss_temp
+  dW /= num_train
+  dW += 2*reg*W
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
--- assignments/2018/assignment1/cs231n/classifiers/softmax.py
+++ assignments/2018/assignment1/cs231n/classifiers/softmax.py
@@ -1,6 +1,12 @@
 import numpy as np
 from random import shuffle
 
+def softmax(x):
+    shiftx = x - np.max(x)
+    exps = np.exp(shiftx)
+    return exps / (np.sum(exps)+1e-5)
+
+
 def softmax_loss_naive(W, X, y, reg):
   """
   Softmax loss function, naive implementation (with loops)
@@ -29,7 +35,26 @@ def softmax_loss_naive(W, X, y, reg):
   # here, it is easy to run into numeric instability. Don't forget the        #
   # regularization!                                                           #
   #############################################################################
-  pass
+  train_num = X.shape[0]
+  class_num = W.shape[1]
+  for i in range(train_num):
+    score = X[i] @ W
+    softmax_score = softmax(score)
+    loss -= np.log(softmax_score[y[i]])
+    
+    delta_softmax = -softmax_score * softmax_score[y[i]]
+    delta_softmax[y[i]] += softmax_score[y[i]]
+    
+    for j in range(class_num):
+        dW[:,j] += softmax_score[j] * X[i,:]
+        if y[i] == j:
+            dW[:,j] -= X[i,:]
+    
+  loss /= train_num
+  loss +=  reg *np.sum(W*W)
+    
+  dW /= train_num
+  dW += 2*reg*W
   #############################################################################
   #                          END OF YOUR CODE                                 #
   #############################################################################
@@ -53,7 +78,24 @@ def softmax_loss_vectorized(W, X, y, reg):
   # here, it is easy to run into numeric instability. Don't forget the        #
   # regularization!                                                           #
   #############################################################################
-  pass
+  train_num = X.shape[0]
+  scores = X @ W
+  shift_score = scores - np.amax(scores,axis=1,keepdims=True)
+  exps = np.exp(shift_score)
+  softmax_scores = exps / (np.sum(exps,axis=1,keepdims=True)+1e-5)
+  
+  log_loss = -np.log(softmax_scores[np.arange(train_num),y])
+  loss = np.mean(log_loss)
+  loss += reg *np.sum(W*W)
+
+  dloss = softmax_scores
+  dloss[np.arange(train_num),y] -= 1
+  dW = X.T @ dloss
+  
+  dW /= train_num
+  dW += 2*reg*W
+  
+  
   #############################################################################
   #                          END OF YOUR CODE                                 #
   #############################################################################
