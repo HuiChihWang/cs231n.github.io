b73e15ca18ea516baf442a919e83ed1ea36f871a
HuiChihWang <taya87136@gmail.com>
2018-10-31 05:09:37 +0000

    fininsh loss and grad of two layer net

--- assignments/2018/spring1718_assignment2_v2/cs231n/classifiers/fc_net.py
+++ assignments/2018/spring1718_assignment2_v2/cs231n/classifiers/fc_net.py
@@ -47,7 +47,10 @@ class TwoLayerNet(object):
         # and biases using the keys 'W1' and 'b1' and second layer                 #
         # weights and biases using the keys 'W2' and 'b2'.                         #
         ############################################################################
-        pass
+        self.params['W1'] = np.random.normal(0,weight_scale,(input_dim,hidden_dim))
+        self.params['W2'] = np.random.normal(0,weight_scale,(hidden_dim,num_classes))
+        self.params['b1'] = np.zeros(hidden_dim)
+        self.params['b2'] = np.zeros(num_classes)
         ############################################################################
         #                             END OF YOUR CODE                             #
         ############################################################################
@@ -77,7 +80,13 @@ class TwoLayerNet(object):
         # TODO: Implement the forward pass for the two-layer net, computing the    #
         # class scores for X and storing them in the scores variable.              #
         ############################################################################
-        pass
+        N = X.shape[0]
+        W1,b1 = self.params['W1'],self.params['b1']
+        W2,b2 = self.params['W2'],self.params['b2']
+        reg = self.reg
+        
+        hidden, cache_hidden = affine_relu_forward(X, W1, b1)
+        scores, cache_scores = affine_forward(hidden, W2, b2)
         ############################################################################
         #                             END OF YOUR CODE                             #
         ############################################################################
@@ -97,7 +106,30 @@ class TwoLayerNet(object):
         # automated tests, make sure that your L2 regularization includes a factor #
         # of 0.5 to simplify the expression for the gradient.                      #
         ############################################################################
-        pass
+        # softmax loss         
+        exps = np.exp(scores)
+        softmax_scores = exps / (np.sum(exps,axis=1,keepdims=True)+1e-5)
+
+        log_loss = -np.log(softmax_scores[np.arange(N),y])
+        loss = np.mean(log_loss)
+        loss += 0.5*reg*(np.sum(W1*W1) + np.sum(W2*W2))
+        
+        # softmax dloss
+        dscores = softmax_scores
+        dscores[np.arange(N),y] -= 1
+        dscores /= N
+        
+        # grad update        
+        dhidden, dW2, db2 = affine_backward(dscores,cache_scores)
+        dX, dW1, db1 = affine_relu_backward(dhidden,cache_hidden)
+
+        # reg term
+        dW2 += reg*W2
+        dW1 += reg*W1
+        
+        # save grad updates to grads
+        grads['W2'], grads['b2'] = dW2,db2
+        grads['W1'], grads['b1'] = dW1,db1
         ############################################################################
         #                             END OF YOUR CODE                             #
         ############################################################################
